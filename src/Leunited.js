6479591c69b80ff99fb6ca1ba8fadbc954c897d6
Ex-hax <watchara.c.lee@gmail.com>
2022-12-24 21:35:47 +0700

    L24122022: event player join and player chat bug fix

--- src/Leunited.js
+++ src/Leunited.js
@@ -38,11 +38,7 @@ var is_event_start = false;
 /**/
 
 game.subscribeToEvent('playerJoins',(data,context) => {
-	if (game.players[context.playerId].isNpc != true){
-		if ((game.players[context.playerId]['affiliation'] != '' || game.players[context.playerId]['affiliation'] == '') && game.players[context.playerId]['description'] == ''){
-			var old_player_info = JSON.stringify({HP:100, MANA:100, L$:100});
-			game.setDescription(old_player_info,context.playerId);
-		}
+	if (context.playerId != 'pdZnTdb7VShWtOz6F2xdVAJ4nOs2'){
 		game.engine.sendAction({
 		$case: "chat",
 			chat: { 
@@ -80,207 +76,210 @@ game.subscribeToEvent("playerMoves", (data, context) => {
 });
 
 game.subscribeToEvent("playerChats", (data, context) => {
-	const message = data.playerChats;
-	if (message.messageType === "DM") {
-		switch (message.contents.toLowerCase()) {
-			case "/help":
-				game.engine.sendAction({
-					$case: "chat",
-						chat: { 
-							chatRecipient: data.playerChats.senderId,
-							contents: `1. To be our guild member type /regis.
-							2. Type /leave for left guild.
-							3. Type /info to get player info.`,
-							localPlayerIds: [],
-							mapId: 'blank',
-						}
-				});
-				break;
-			case "/regis":
-				if (context.player.affiliation != 'ally'){
-					game.setAffiliation('ally',data.playerChats.senderId);
-					var new_player_info = JSON.stringify({HP:100, MANA:100, L$:100});
-					game.setDescription(new_player_info,data.playerChats.senderId);
-					game.notify(`${context?.player?.name ?? context.playerId} join Leunited!!`);
+	if (context.playerId != 'pdZnTdb7VShWtOz6F2xdVAJ4nOs2'){
+		const message = data.playerChats;
+		console.log(data);
+		if (message.messageType === "DM") {
+			switch (message.contents.toLowerCase()) {
+				case "/help":
 					game.engine.sendAction({
 						$case: "chat",
 							chat: { 
 								chatRecipient: data.playerChats.senderId,
-								contents: `Successful register!
-								Welcome to our guild.
-								HP: 100 MANA: 100 L$: 100`,
+								contents: `1. To be our guild member type /regis.
+								2. Type /leave for left guild.
+								3. Type /info to get player info.`,
 								localPlayerIds: [],
 								mapId: 'blank',
 							}
 					});
-				}
-				else {
+					break;
+				case "/regis":
+					if (context.player.affiliation != 'ally'){
+						game.setAffiliation('ally',data.playerChats.senderId);
+						var new_player_info = JSON.stringify({HP:100, MANA:100, L$:100});
+						game.setDescription(new_player_info,data.playerChats.senderId);
+						game.notify(`${context?.player?.name ?? context.playerId} join Leunited!!`);
+						game.engine.sendAction({
+							$case: "chat",
+								chat: { 
+									chatRecipient: data.playerChats.senderId,
+									contents: `Successful register!
+									Welcome to our guild.
+									HP: 100 MANA: 100 L$: 100`,
+									localPlayerIds: [],
+									mapId: 'blank',
+								}
+						});
+					}
+					else {
+						game.engine.sendAction({
+							$case: "chat",
+								chat: { 
+									chatRecipient: data.playerChats.senderId,
+									contents: 'You have been our guild member!',
+									localPlayerIds: [],
+									mapId: 'blank',
+								}
+						});
+					}
+					break;
+				case "/info":
+					if (game.players[data.playerChats.senderId]['affiliation'] != ''){
+						var p_info_request = game.players[data.playerChats.senderId]['description'];
+						p_info_request = JSON.parse(p_info_request);
+						game.engine.sendAction({
+							$case: "chat",
+								chat: { 
+									chatRecipient: data.playerChats.senderId,
+									contents: `ID: ${data.playerChats.senderId}
+									HP: ${p_info_request['HP']}
+									MANA: ${p_info_request['MANA']}
+									L$: ${p_info_request['L$']}`,
+									localPlayerIds: [],
+									mapId: 'blank',
+								}
+						});
+					}
+					else {
+						game.engine.sendAction({
+							$case: "chat",
+								chat: { 
+									chatRecipient: data.playerChats.senderId,
+									contents: `Please register ❗`,
+									localPlayerIds: [],
+									mapId: 'blank',
+								}
+						});
+					}
+					break;
+				case "/market":
 					game.engine.sendAction({
 						$case: "chat",
 							chat: { 
 								chatRecipient: data.playerChats.senderId,
-								contents: 'You have been our guild member!',
+								contents: `Welcome to our guild market place!
+								To receive buff press x
+								1. Speed-buff: cost 20 L$.
+								2. Teleport to gate: cost 50 L$.`,
 								localPlayerIds: [],
 								mapId: 'blank',
 							}
 					});
-				}
-				break;
-			case "/info":
-				if (game.players[data.playerChats.senderId]['affiliation'] != ''){
-					var p_info_request = game.players[data.playerChats.senderId]['description'];
-					p_info_request = JSON.parse(p_info_request);
+					break;
+				case "/leave":
+					game.setAffiliation('',data.playerChats.senderId);
+					game.setDescription('',data.playerChats.senderId);
 					game.engine.sendAction({
 						$case: "chat",
 							chat: { 
 								chatRecipient: data.playerChats.senderId,
-								contents: `ID: ${data.playerChats.senderId}
-								HP: ${p_info_request['HP']}
-								MANA: ${p_info_request['MANA']}
-								L$: ${p_info_request['L$']}`,
+								contents: `You are not guild member for now
+								1. Your HP MANA and L$ will reset to default.
+								2. you will lose all progress and items.`,
 								localPlayerIds: [],
 								mapId: 'blank',
 							}
 					});
-				}
-				else {
-					game.engine.sendAction({
-						$case: "chat",
-							chat: { 
-								chatRecipient: data.playerChats.senderId,
-								contents: `Please register ❗`,
-								localPlayerIds: [],
-								mapId: 'blank',
-							}
-					});
-				}
-				break;
-			case "/market":
-				game.engine.sendAction({
-					$case: "chat",
-						chat: { 
-							chatRecipient: data.playerChats.senderId,
-							contents: `Welcome to our guild market place!
-							To receive buff press x
-							1. Speed-buff: cost 20 L$.
-							2. Teleport to gate: cost 50 L$.`,
-							localPlayerIds: [],
-							mapId: 'blank',
-						}
-				});
-				break;
-			case "/leave":
-				game.setAffiliation('',data.playerChats.senderId);
-				game.setDescription('',data.playerChats.senderId);
-				game.engine.sendAction({
-					$case: "chat",
-						chat: { 
-							chatRecipient: data.playerChats.senderId,
-							contents: `You are not guild member for now
-							1. Your HP MANA and L$ will reset to default.
-							2. you will lose all progress and items.`,
-							localPlayerIds: [],
-							mapId: 'blank',
-						}
-				});
-				game.teleport('blank',50,72,data.playerChats.senderId);
-				break;
-			case "/start":
-				if (is_event_start == false){
-					if (game.players[data.playerChats.senderId]['map'] == 'RdrF5O3qyQ91gFAdofHTv'){
-						const all_pid_key_join = Object.keys(game.players);
-						for (var i=0;i<all_pid_key_join.length;i++){
-							if (game.players[all_pid_key_join[i]]['map'] == 'RdrF5O3qyQ91gFAdofHTv'){
-								if (killer_id.length == 0 && !killer_id.includes(data.playerChats.senderId)){
-									killer_id.push(data.playerChats.senderId);
-									game.teleport('RdrF5O3qyQ91gFAdofHTv',82,9,data.playerChats.senderId);
-									game.setSpeedModifier(2,data.playerChats.senderId);
-									console.log(killer_id);
-									if (data.playerChats.senderId == all_pid_key_join[i]){
-										console.log('Already added.');
+					game.teleport('blank',50,72,data.playerChats.senderId);
+					break;
+				case "/start":
+					if (is_event_start == false){
+						if (game.players[data.playerChats.senderId]['map'] == 'RdrF5O3qyQ91gFAdofHTv'){
+							const all_pid_key_join = Object.keys(game.players);
+							for (var i=0;i<all_pid_key_join.length;i++){
+								if (game.players[all_pid_key_join[i]]['map'] == 'RdrF5O3qyQ91gFAdofHTv'){
+									if (killer_id.length == 0 && !killer_id.includes(data.playerChats.senderId)){
+										killer_id.push(data.playerChats.senderId);
+										game.teleport('RdrF5O3qyQ91gFAdofHTv',82,9,data.playerChats.senderId);
+										game.setSpeedModifier(2,data.playerChats.senderId);
+										console.log(killer_id);
+										if (data.playerChats.senderId == all_pid_key_join[i]){
+											console.log('Already added.');
+										}
+										else{
+											players_join.push(all_pid_key_join[i]);
+										}
 									}
-									else{
-										players_join.push(all_pid_key_join[i]);
+									else {
+										if(killer_id.includes(all_pid_key_join[i]) == false){
+											players_join.push(all_pid_key_join[i]);
+											game.setSpeedModifier(1,all_pid_key_join[i]);
+											game.teleport('RdrF5O3qyQ91gFAdofHTv',50,50,all_pid_key_join[i]);
+											console.log(players_join);
+										}
+										
 									}
 								}
-								else {
-									if(killer_id.includes(all_pid_key_join[i]) == false){
-										players_join.push(all_pid_key_join[i]);
-										game.setSpeedModifier(1,all_pid_key_join[i]);
-										game.teleport('RdrF5O3qyQ91gFAdofHTv',50,50,all_pid_key_join[i]);
-										console.log(players_join);
-									}
-									
-								}
 							}
-						}
-						if (players_join.length == 0){
-							killer_id = [];
-							game.setSpeedModifier(1,data.playerChats.senderId);
-							game.teleport('RdrF5O3qyQ91gFAdofHTv',50,50,data.playerChats.senderId);
-							game.engine.sendAction({
-								$case: "chat",
-									chat: { 
-										chatRecipient: data.playerChats.senderId,
-										contents: `The game can't start due to 
-										no survivor. Please wait for another
-										or can leave by respawn.`,
-										localPlayerIds: [],
-										mapId: 'RdrF5O3qyQ91gFAdofHTv',
-									}
-							});
-						}
-						else{
-							is_event_start = true;
-							sur_win_percent = players_join.length + killer_id.length;
-							if (sur_win_percent > 500){
-								sur_win_percent = 500;
+							if (players_join.length == 0){
+								killer_id = [];
+								game.setSpeedModifier(1,data.playerChats.senderId);
+								game.teleport('RdrF5O3qyQ91gFAdofHTv',50,50,data.playerChats.senderId);
+								game.engine.sendAction({
+									$case: "chat",
+										chat: { 
+											chatRecipient: data.playerChats.senderId,
+											contents: `The game can't start due to 
+											no survivor. Please wait for another
+											or can leave by respawn.`,
+											localPlayerIds: [],
+											mapId: 'RdrF5O3qyQ91gFAdofHTv',
+										}
+								});
+							}
+							else{
+								is_event_start = true;
+								sur_win_percent = players_join.length + killer_id.length;
+								if (sur_win_percent > 500){
+									sur_win_percent = 500;
+								}
 							}
 						}
 					}
-				}
-				else {
-					console.log('Event has been started.');
-					game.engine.sendAction({
-					$case: "chat",
-						chat: { 
-							chatRecipient: data.playerChats.senderId,
-							contents: `Event has been started.`,
-							localPlayerIds: [],
-							mapId: 'blank',
-						}
-					});
-				}
-				break;
-			case "/stop":
-				killer_id = [];
-				players_join = [];
-				gens = [
-					{'id': 'gen_a', 'gen_a': 0, 'is_trigger': false, 'is_break': false},
-					{'id': 'gen_b', 'gen_b': 0, 'is_trigger': false, 'is_break': false},
-					{'id': 'gen_c', 'gen_c': 0, 'is_trigger': false, 'is_break': false},
-					{'id': 'gen_d', 'gen_d': 0, 'is_trigger': false, 'is_break': false},
-					{'id': 'gen_e', 'gen_e': 0, 'is_trigger': false, 'is_break': false},
-				];
-				const all_pid_key_left = Object.keys(game.players);
-				for (var i=0;i<all_pid_key_left.length;i++){
-					if(game.players[all_pid_key_left[i]]['map'] == 'RdrF5O3qyQ91gFAdofHTv'){
-						game.teleport('blank',56,44,all_pid_key_left[i]);
-						game.setSpeedModifier(1,all_pid_key_left[i]);
+					else {
+						console.log('Event has been started.');
+						game.engine.sendAction({
+						$case: "chat",
+							chat: { 
+								chatRecipient: data.playerChats.senderId,
+								contents: `Event has been started.`,
+								localPlayerIds: [],
+								mapId: 'blank',
+							}
+						});
 					}
-				}
-				is_event_start = false;
-				break;
-			default:
-				game.engine.sendAction({
-					$case: "chat",
-						chat: { 
-							chatRecipient: data.playerChats.senderId,
-							contents: 'What do you mean?',
-							localPlayerIds: [],
-							mapId: 'blank',
+					break;
+				case "/stop":
+					killer_id = [];
+					players_join = [];
+					gens = [
+						{'id': 'gen_a', 'gen_a': 0, 'is_trigger': false, 'is_break': false},
+						{'id': 'gen_b', 'gen_b': 0, 'is_trigger': false, 'is_break': false},
+						{'id': 'gen_c', 'gen_c': 0, 'is_trigger': false, 'is_break': false},
+						{'id': 'gen_d', 'gen_d': 0, 'is_trigger': false, 'is_break': false},
+						{'id': 'gen_e', 'gen_e': 0, 'is_trigger': false, 'is_break': false},
+					];
+					const all_pid_key_left = Object.keys(game.players);
+					for (var i=0;i<all_pid_key_left.length;i++){
+						if(game.players[all_pid_key_left[i]]['map'] == 'RdrF5O3qyQ91gFAdofHTv'){
+							game.teleport('blank',56,44,all_pid_key_left[i]);
+							game.setSpeedModifier(1,all_pid_key_left[i]);
 						}
-				});
+					}
+					is_event_start = false;
+					break;
+				default:
+					game.engine.sendAction({
+						$case: "chat",
+							chat: { 
+								chatRecipient: data.playerChats.senderId,
+								contents: 'What do you mean?',
+								localPlayerIds: [],
+								mapId: 'blank',
+							}
+					});
+			}
 		}
 	}
 });
